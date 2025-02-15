b966432a3ab2153a5c9059e4e2ed4309f40bb7e0
midilab <contact@midilab.co>
2025-02-15 09:01:40 -0300

    getting back default isr with no NOBLOCK

--- platforms/avr.h
+++ platforms/avr.h
@@ -12,9 +12,9 @@ namespace uctrl {
 #define AVR_CLOCK_FREQ	16000000
 
 #if defined(__AVR_ATmega32U4__)	
-ISR(TIMER3_COMPA_vect, ISR_NOBLOCK) 
+ISR(TIMER3_COMPA_vect) 
 #else
-ISR(TIMER2_COMPA_vect, ISR_NOBLOCK) 
+ISR(TIMER2_COMPA_vect) 
 #endif
 {
 	uCtrlHandler();
